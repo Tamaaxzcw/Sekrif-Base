1ce287db6045dbd891487439ab5892eb8ef23f02
TamaaXzcw <3kdiasforyou@gmail.com>
2025-08-26 23:39:28 +0800

    add files via upload
    add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+export default {
+  // Edit nama
+  namebot: "SekaiVerse",
+  nameowner: "Rieze The Great",
+
+  // Edit Nomer
+  nomerowner: "6281547107318",
+  nomerbot: "6281547107318", // nomor pairing
+
+  // Moderator
+  mods: ["6281547107318", "62xxxxxxx"],
+
+  // Setting Lain
+  pairing: true, // true = pairing code, false = QR
+  session: "./session", // folder simpan session
+  mode: "work", // sleep | work | grup
+
+  // Core
+  prefix: ".",
+  plugins: "./plugins",
+  database: true
+}
